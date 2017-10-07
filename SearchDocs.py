c6179862817cc7ac0b337dc4bd0399d502382aea
arkainoh <arkainoh@gmail.com>
2017-10-07 16:33:48 +0900

    add tokenize()

--- SearchDocs.py
+++ SearchDocs.py
@@ -1,6 +1,22 @@
 import nltk
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords as sw
 
 class Tools:
-	def __init__(self):
-		
+	def tokenize(self, inputstr, onlyalpha = True, stopwords = False, stemmer = True):
+		inputstr = inputstr.lower()
+		tokens = nltk.word_tokenize(inputstr)
+
+		if(onlyalpha):
+			tokens = [i for i in tokens if i.isalpha()]
+
+		if(not stopwords):
+			stpwrds = set(sw.words('english'))
+			tokens = [i for i in tokens if i not in stpwrds]
+
+		if(stemmer):
+			stmr = nltk.stem.porter.PorterStemmer()
+			tokens = [stmr.stem(i) for i in tokens]
+
+		return tokens
+
+tools = Tools()
